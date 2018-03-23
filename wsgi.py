599fbe28e5907cfcd6fc3940ea0d15c97ac07b63
Niharika Gupta <gupta.526@buckeyemail.osu.edu>
2018-03-23 14:25:40 -0400

    changed route added new route

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,11 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Testing Hello World!"
+
+@application.route("/new_route"):
+def new_route():
+	return "testing second route"
 
 if __name__ == "__main__":
     application.run()
