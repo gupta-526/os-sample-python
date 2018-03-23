0a88b44bf1e1ed516f99c304570f3585d2b3d3df
Niharika Gupta <gupta.526@buckeyemail.osu.edu>
2018-03-23 14:32:11 -0400

    removed new route

--- wsgi.py
+++ wsgi.py
@@ -5,9 +5,9 @@ application = Flask(__name__)
 def hello():
     return "Testing Hello World!"
 
-@application.route("/new_route"):
-def new_route():
-	return "testing second route"
+# @application.route("/new_route"):
+# def new_route():
+# 	return "testing second route"
 
 if __name__ == "__main__":
     application.run()
